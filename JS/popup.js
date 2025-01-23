d308e8b4dd633f1c23c9f5565ebba150ccc25082
Vitaliy Fedytnyk <vitalikfedytnyk@gmail.com>
2025-01-23 22:49:06 +0200

    popup

--- JS/popup.js
+++ JS/popup.js
@@ -1,9 +1,21 @@
 const popup = document.getElementById('popup');
 
 function openPopup() {
-    popup.classList.add("active")
+    popup.classList.add("active");
 }
 
 function closePopup() {
-    popup.classList.remove("active")
+    popup.classList.remove("active");
 }
+
+popup.addEventListener('click', function(event) {
+    if (event.target === popup) {
+        closePopup();
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closePopup();
+    }
+});
